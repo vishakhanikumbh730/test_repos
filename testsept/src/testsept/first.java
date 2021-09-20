965f2641ca7dd03dce7b5199bae8fcb12bd4dd8a
vishakhanikumbh730 <vnikumbh@LIN59017926.corp.capgemini.com>
2021-09-20 11:09:32 +0530

    sum added in show method

--- testsept/src/testsept/first.java
+++ testsept/src/testsept/first.java
@@ -9,6 +9,13 @@ public class first {
 	}
 	public static void show()
 	{
+		int[] arr = {1,2,3,4,4};
+		int sum=0;
+		for(int n:arr)
+		{
+			sum+=n;
+			System.out.println(arr[i]);
+		}
 		System.out.println("show method here");
 	}
 }
