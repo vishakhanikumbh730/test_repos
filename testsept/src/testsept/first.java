366f3bdbf7387a1b38a58d90c353320fbc848de6
vishakhanikumbh730 <vnikumbh@LIN59017926.corp.capgemini.com>
2021-09-20 10:52:44 +0530

    show method added

--- testsept/src/testsept/first.java
+++ testsept/src/testsept/first.java
@@ -5,6 +5,10 @@ public class first {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		show();
 	}
-
+	public static void show()
+	{
+		System.out.println("show method here");
+	}
 }
